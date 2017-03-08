b84ec3f10a5702045a66251e9546433f9745e704
peteyk <peteyk@u.washington.edu>
2017-03-08 11:02:03 -0800

    Add new classes from machinelearning branch

---
+++ src/org/rhwlab/machinelearning/DivisionSet.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.rhwlab.machinelearning;
+
+import java.io.File;
+import org.rhwlab.dispim.nucleus.Nucleus;
+
+/**
+ *
+ * @author gevirl
+ */
+public class DivisionSet extends TrainingSet {
+
+    @Override
+    public void addNucleiFrom(File sessionXML, double localRegion) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Comparable[] formDataVector(String cl, Nucleus source, Nucleus[] next) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String[] getLabels() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
